7e75a4511520170829caeb77266d2aa802bb2e46
Nikolaj Kappler <info@nkappler.de>
2024-12-10 23:06:14 +0100

    added timestamp output

--- raytracer.cpp
+++ raytracer.cpp
@@ -7,6 +7,8 @@
 #include "material.h"
 
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 color ray_color(const ray &r, const hittable &world, int depth)
 {
@@ -36,7 +38,7 @@ hittable_list random_scene()
 	hittable_list world;
 
 	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+	world.add(make_shared<sphere>(point3(0, -10000, 0), 10000, ground_material));
 
 	for (int a = -11; a < 11; a++)
 	{
@@ -88,14 +90,15 @@ hittable_list random_scene()
 
 int main()
 {
+	auto start = std::chrono::system_clock::now();
 
 	// Image
 
 	const auto aspect_ratio = 3.0 / 2.0;
 	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_bounces = 10;
+	const int samples_per_pixel = 10;
+	const int max_bounces = 6;
 
 	// World
 
@@ -107,7 +110,7 @@ int main()
 	point3 lookat(0, 0, 0);
 	vec3 vup(0, 1, 0);
 	auto dist_to_focus = 10.0;
-	auto aperture = 0.1;
+	auto aperture = 0.05;
 
 	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
@@ -133,4 +136,14 @@ int main()
 	}
 
 	std::cerr << "\nDone.\n";
+
+	// Some computation here
+	auto end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+	std::cerr << "finished computation at " << std::ctime(&end_time)
+			  << "elapsed time: " << elapsed_seconds.count() << "s"
+			  << std::endl;
 }
